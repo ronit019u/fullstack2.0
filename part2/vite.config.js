1b8a0eb949709f8291edf18fc343a89581ba8655
ronit <uttkarshbartwal@gmail.com>
2024-03-01 11:15:39 +0530

    bafef

--- part2/vite.config.js
+++ part2/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://fullstack2-0.onrender.com',
         changeOrigin: true,
       },
     }
